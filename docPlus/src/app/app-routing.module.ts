6599c2ad740a41fae52a5ce1bb5d37d6fb9fbce4
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2022-09-19 01:39:43 -0300

    :sparkles: feat: 1.003: Tela de Login

---
+++ docPlus/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from "@angular/router";
+
+import { LoginComponent } from './login/login.component';
+//import { SobreComponent } from './sobre/sobre.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent}
+  //{ path: 'sobre', component: SobreComponent}
+  ];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- docPlus/src/app/app.module.ts
+++ docPlus/src/app/app.module.ts
@@ -1,14 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+//import { SobreComponent } from './sobre/sobre.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
+    //SobreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
