bb7349ed39dbaecedec5273d6230a3f6641dc778
Gabriel Farina <gabrielfarina2358@gmail.com>
2022-09-28 19:27:40 -0300

    :sparkles:feat:2.002:add botao para exclusão de paciente

--- docPlus/src/app/administrativo/pacientes/pacientes.component.ts
+++ docPlus/src/app/administrativo/pacientes/pacientes.component.ts
@@ -12,31 +12,39 @@ export class PacientesComponent implements OnInit {
   ngOnInit(): void {
     this.gerarLista()
 
+    let lista = document.getElementsByClassName("informacaoPaciente")
     $(document.getElementById("salvarDados") as HTMLButtonElement).click(function(e){
 
-//------------------------------------------gravação de alterações de dados------------------------------------------
-      for (let index = 0; index < 10; index++) {
-        let lista = document.getElementById("paciente" + (index + 1))
-        let nomePaciente: string = (lista?.children[0] as HTMLInputElement).value
-        let dataNascPaciente: string = (lista?.children[1] as HTMLInputElement).value
-        let sexoPaciente: string =  (lista?.children[2] as HTMLInputElement).value
-        let cpfPaciente: number = Number((lista?.children[3] as HTMLInputElement).value)
-        let telefonePaciente: number = Number((lista?.children[4] as HTMLInputElement).value)
-        let emailPaciente = (lista?.children[5] as HTMLInputElement).value
-        let enderecoPaciente = (lista?.children[6] as HTMLInputElement).value
-        let planoPaciente = (lista?.children[7] as HTMLInputElement).value
+    //------------------------------------------gravação de alterações de dados------------------------------------------
+      for (let index = 0; index < lista?.length; index++) {
+        let nomePaciente: string = (lista?.[index].children[0] as HTMLInputElement).value
+        let dataNascPaciente: string = (lista?.[index].children[1] as HTMLInputElement).value
+        let sexoPaciente: string =  (lista?.[index].children[2] as HTMLInputElement).value
+        let cpfPaciente: string = (lista?.[index].children[3] as HTMLInputElement).value
+        let telefonePaciente: string = (lista?.[index].children[4] as HTMLInputElement).value
+        let emailPaciente = (lista?.[index].children[5] as HTMLInputElement).value
+        let enderecoPaciente = (lista?.[index].children[6] as HTMLInputElement).value
+        let planoPaciente = (lista?.[index].children[7] as HTMLInputElement).value
 
-        if (nomePaciente != undefined) {
+        if (nomePaciente != undefined && nomePaciente != 'undefined') {
           //Gravar Dados
           console.log(nomePaciente + " - " + dataNascPaciente + " - " + sexoPaciente + " - " + cpfPaciente + " - "
           + telefonePaciente + " - " + emailPaciente + " - " + enderecoPaciente + " - " + planoPaciente);
         }
-      }    
+      }
     });
+    //-------------------------------------Excluir Paciente-----------------------------------------
+    $(document.getElementsByClassName("btnExcluir") as HTMLCollection).click(function(e){
+      let numBtnPressed: number = Number(e.target.attributes[0].value.split(" ")[1]);
+      console.log((lista?.[numBtnPressed - 1] as HTMLDivElement));
+      (lista?.[numBtnPressed - 1] as HTMLDivElement).setAttribute('hidden', 'true');
+      ((lista?.[numBtnPressed - 1] as HTMLDivElement).children[0] as HTMLInputElement).setAttribute('value', 'undefined')
+
+    })
   }
 
   gerarLista(){
-    for (let index = 0; index < 10; index++) {
+    for (let index = 0; index < 5; index++) {
 
       let lista = document.getElementById("listaPacientes") as HTMLElement
       let nomePaciente: string = 'Gabriel'
@@ -44,10 +52,10 @@ export class PacientesComponent implements OnInit {
       let sexoPaciente: string =  'M'
       let cpfPaciente: string = '999.999.999-99';
       let telefonePaciente: string = '(49)999999999'
-      let emailPaciente = 'teste@gmail.com'
-      let enderecoPaciente = 'Ipumirim, SC'
-      let planoPaciente = 'São Camilo'
-      let selectSexoPaciente
+      let emailPaciente: string = 'teste@gmail.com'
+      let enderecoPaciente: string = 'Ipumirim, SC'
+      let planoPaciente: string = 'São Camilo'
+      let selectSexoPaciente : string
 
       if (sexoPaciente == 'M') {
         selectSexoPaciente = "<select name='nmSexoPaciente' class='sexoPaciente m-1'>"+
@@ -62,7 +70,7 @@ export class PacientesComponent implements OnInit {
                               "</select>"
       }
 
-      lista.innerHTML += "<div class='informacaoPaciente border border-dark m-2' id='paciente"+ (index + 1) +"'>" + 
+      lista.innerHTML += "<div class='informacaoPaciente border border-dark m-2' id='"+(index + 1)+"'>" + 
                         "<input type='text' class='nomePaciente m-1' placeholder='Nome...' value='" + nomePaciente + "'>" +
                         "<input type='date' class='dataNascPaciente m-1' value='" + dataNascPaciente + "'>" + selectSexoPaciente +
                         "<input type='string' class='cpfPaciente m-1' placeholder='CPF...' value='" + cpfPaciente + "'>" +
@@ -70,6 +78,7 @@ export class PacientesComponent implements OnInit {
                         "<input type='email' class='emailPacinete m-1' placeholder='Email...' value='" + emailPaciente + "'>" +
                         "<input type='text' class='enderecoPaciente m-1' placeholder='Endereço...' value='" + enderecoPaciente + "'>" +
                         "<input type='text' class='planoPaciente m-1' placeholder='Plano de Saúde...' value='" + planoPaciente + "'>" +
+                        "<button class='btnExcluir "+(index + 1)+"'>Excluir Paciente</button>" +
                         "</div>"
     }
   }  
